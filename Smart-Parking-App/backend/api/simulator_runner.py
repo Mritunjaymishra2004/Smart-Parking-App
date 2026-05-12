1b3d9d0b38d7dd6d74b779bca9b4c6d98d60b01d
Mritunjay Mishra <mritunjaymishra7607197286@gmail.com>
2026-05-12 14:59:26 +0530

    Delete Smart-Parking-App directory

--- Smart-Parking-App/backend/api/simulator_runner.py
+++
@@ -1,9 +0,0 @@
-import time
-from .vehicle_simulator import move_vehicles
-
-while True:
-    try:
-        move_vehicles()
-    except Exception as e:
-        print("Vehicle simulator error:", e)
-    time.sleep(5)
--- Smart-Parking-App/backend/backend/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import api.routing
-
-application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
-        URLRouter(api.routing.websocket_urlpatterns)
-    ),
-})
